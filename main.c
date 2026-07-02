05d816423a6ccc8578ac59fd0440910f3998fa53
Lyheang <lyheangleng369@gmail.com>
2026-07-02 12:55:38 +0700

    Initial commit

--- main.c
+++ main.c
@@ -1,5 +1,47 @@
 #include <stdio.h>
-int main(){
-    printf("Sal strawberry");
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/*--- Constants Values ---*/
+
+#define MAX_PRODUCTS  50
+#define MAX_CART      20
+#define PRODUCTS_FILE "products.dat"
+#define SALES_FILE    "sales.dat"
+#define TAX_RATE      0.10
+
+/*--- Structs ---*/
+
+typedef struct {
+    int id;
+    char name[40];
+    double price;
+    int quantity;
+} Product;
+
+typedef struct {
+    int product_id;
+    char name[40];
+    double unit_price;
+    int qty;
+    double subtotal;
+} CartItem;
+
+typedef struct {
+    char   date[20];
+    double total_before_discount;
+    double discount_amount;
+    double tax_amount;
+    double grand_total;
+    double payment;
+    double change;
+} SaleRecord;
+
+
+int main() {
+
+
+
     return 0;
 }
